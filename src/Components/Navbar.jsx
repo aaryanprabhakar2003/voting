a0ad59ffde3aeb40923a9d36f22774590691d4b6
Aaryan Prabhakar <aaryanprabhakar07@gmail.com>
2024-04-16 21:49:33 +0530

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,24 +6,26 @@ const Navbar = () => {
     <div class=" mx-auto flex max-w-screen-xl flex-wrap items-center justify-between ">
       <div className="fixed bottom-4 left-1/2 z-50 h-16 w-[95%] max-w-lg -translate-x-1/2 rounded-full border border-gray-200 bg-white px-2 md:px-0 dark:border-gray-600 dark:bg-gray-700 ">
         <div className="mx-auto grid h-full max-w-lg grid-cols-5 ">
-         
-            <button
-              data-tooltip-target="tooltip-home"
-              type="button"
-              className="group inline-flex flex-col items-center justify-center rounded-s-full px-5 hover:bg-gray-50 dark:hover:bg-gray-800"
-            >
-              <svg
-                className=" mb-1 h-auto w-5 text-gray-500 group-hover:text-blue-600 dark:text-gray-400 dark:group-hover:text-blue-500 "
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                viewBox="0 0 20 20"
+          <Link to="/">
+            <div className="flex items-center justify-center">
+              <button
+                data-tooltip-target="tooltip-home"
+                type="button"
+                className="group mt-5 inline-flex flex-col items-center justify-center rounded-s-full px-5 dark:hover:bg-gray-800"
               >
-                <path d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z" />
-              </svg>
-              <span className="sr-only">Home</span>
-            </button>
-          
+                <svg
+                  className=" mb-1 h-auto w-5 text-gray-500 group-hover:text-blue-600 dark:text-gray-400 dark:group-hover:text-blue-500 "
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z" />
+                </svg>
+                <span className="sr-only">Home</span>
+              </button>
+            </div>
+          </Link>
 
           <div
             id="tooltip-home"
@@ -134,22 +136,33 @@ const Navbar = () => {
             Settings
             <div className="tooltip-arrow" data-popper-arrow />
           </div>
+          <Link to="/contact">
           <button
             data-tooltip-target="tooltip-profile"
             type="button"
-            className="group inline-flex flex-col items-center justify-center rounded-e-full px-5 hover:bg-gray-50 dark:hover:bg-gray-800"
+            className=" mt-5 group inline-flex flex-col items-center justify-center rounded-e-full px-5 dark:hover:bg-gray-800"
           >
             <svg
-              className="mb-1 h-5 w-5 text-gray-500 group-hover:text-blue-600 dark:text-gray-400 dark:group-hover:text-blue-500"
+              class=" w-6 text-gray-800 dark:text-white"
               aria-hidden="true"
               xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 20 20"
+              width="24"
+              height="24"
+              fill="none"
+              viewBox="0 0 24 24"
             >
-              <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z" />
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M7 6H5m2 3H5m2 3H5m2 3H5m2 3H5m11-1a2 2 0 0 0-2-2h-2a2 2 0 0 0-2 2M7 3h11a1 1 0 0 1 1 1v16a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1Zm8 7a2 2 0 1 1-4 0 2 2 0 0 1 4 0Z"
+              />
             </svg>
+
             <span className="sr-only">Profile</span>
           </button>
+          </Link>
           <div
             id="tooltip-profile"
             role="tooltip"
